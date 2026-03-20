f52e776cd94028e8579bd4b5c69242270555fa54
Кирилл Боев | Icedunter <130907228+IceDunTer@users.noreply.github.com>
2026-03-20 08:22:11 +0300

    Delete lab1_mvc directory

--- lab1_mvc/src/main/java/org/boev/lab1/Main.java
+++
@@ -1,9 +0,0 @@
-package org.boev.lab1;
-
-import javafx.application.Application;
-
-public class Main {
-    public static void main(String[] args) {
-        Application.launch(View.class, args);
-    }
-}
